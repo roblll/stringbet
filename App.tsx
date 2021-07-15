3eb07a60161af1a577e5425bf1f70bb0ce68c04e
rob <lenon.rob@gmail.com>
2021-07-15 18:14:56 -0500

    Update App.tsx to change background color

--- App.tsx
+++ App.tsx
@@ -13,7 +13,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#23A484',
     alignItems: 'center',
     justifyContent: 'center',
   },
