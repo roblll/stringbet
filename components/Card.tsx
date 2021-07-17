bbca628699a42f37276016f880896d73ac345ed2
rob <lenon.rob@gmail.com>
2021-07-16 21:59:33 -0500

    Add Card.tsx

---
+++ components/Card.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+export type Props = {
+  suit: string;
+  rank: number;
+}
+
+const Card: React.FC<Props> = ({ suit, rank }) => {
+  return (
+    <View>
+      <Text>{rank} {suit}</Text>
+    </View>
+  )
+}
