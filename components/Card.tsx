2d2f18146e3da6d395f9e8495558a3299ad16b24
rob <lenon.rob@gmail.com>
2021-07-19 18:08:13 -0500

    Update Card.tsx to use card images

--- components/Card.tsx
+++ components/Card.tsx
@@ -1,31 +1,36 @@
 import React from 'react';
-import { View, Text, StyleSheet, Dimensions } from 'react-native';
+import { View, StyleSheet, Dimensions, Image } from 'react-native';
 
 const cardWidth = Dimensions.get('window').width * 6 / 15;
-const cardHeight = cardWidth * 3.5 / 2.5
+const cardHeight = cardWidth * 1056 / 691;
 
 export type Props = {
   suit: string;
   rank: number;
 }
 
-const Card: React.FC<Props> = ({ suit, rank }) => {
-  return (
-    <View style={styles.container}>
-      <Text>{rank} {suit}</Text>
-    </View>
-  )
-}
-
 const styles = StyleSheet.create({
   container: {
-    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
     width: cardWidth,
     height: cardHeight,
-    borderRadius: 10
   },
+  cardStyle: {
+    width: cardWidth,
+    height: cardHeight,
+  }
 });
+
+const Card: React.FC<Props> = ({ suit, rank }) => {
+  return (
+    <View style={styles.container}>
+      <Image
+        style={styles.cardStyle}
+        source={require('../assets/card-images/AC.png')}
+      />
+    </View>
+  )
+}
 
 export default Card;
