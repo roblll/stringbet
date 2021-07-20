7e242f7eef2ff4b14847bccb57aeb9cb2c7fa877
rob <lenon.rob@gmail.com>
2021-07-20 17:30:30 -0500

    Update App.tsx to add second card and additional styling

--- App.tsx
+++ App.tsx
@@ -8,7 +8,12 @@ export default function App() {
   return (
     <View style={styles.container}>
       <StatusBar style="auto" />
-      <Card rank='A' suit='S' />
+      <View style={styles.headerContainer}></View>
+      <View style={styles.cardsContainer}>
+        <Card rank='A' suit='S' />
+        <Card rank='A' suit='D' />
+      </View>
+      <View style={styles.controlsContainer}></View>
     </View>
   );
 }
@@ -20,4 +25,14 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  headerContainer: {
+    flex: 1,
+  },
+  cardsContainer: {
+    flex: 1,
+    flexDirection: 'row',
+  },
+  controlsContainer: {
+    flex: 1,
+  }
 });
