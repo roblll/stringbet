71af19c3490ab7dc9c139304bbe86d0a44b70153
rob <lenon.rob@gmail.com>
2021-07-20 17:10:10 -0500

    Update Card.tsx to update Props types and rankSuitType and add rest of card images

--- components/Card.tsx
+++ components/Card.tsx
@@ -5,9 +5,11 @@ const cardWidth = Dimensions.get('window').width * 6 / 15;
 const cardHeight = cardWidth * 1056 / 691;
 
 export type Props = {
-  rank: 'A';
+  rank: '2' | '3' | '4' | '5' | '6' | '7' | '8' | '9' | '10' | 'J' | 'Q' | 'K' | 'A';
   suit: 'C' | 'D' | 'H' | 'S';
 }
+
+type rankSuitType = '2C' | '2D' | '2H' | '2S' | '3C' | '3D' | '3H' | '3S' | '4C' | '4D' | '4H' | '4S' | '5C' | '5D' | '5H' | '5S' | '6C' | '6D' | '6H' | '6S' | '7C' | '7D' | '7H' | '7S' | '8C' | '8D' | '8H' | '8S' | '9C' | '9D' | '9H' | '9S' | '10C' | '10D' | '10H' | '10S' | 'JC' | 'JD' | 'JH' | 'JS' | 'QC' | 'QD' | 'QH' | 'QS' | 'KC' | 'KD' | 'KH' | 'KS' | 'AC' | 'AD' | 'AH' | 'AS'
 
 const styles = StyleSheet.create({
   container: {
@@ -23,6 +25,198 @@ const styles = StyleSheet.create({
 });
 
 const cardImages = {
+  '2C': <Image
+    style={styles.cardStyle}
+    source={require('../assets/card-images/2C.png')}
+  />,
+  '2D': <Image
+    style={styles.cardStyle}
+    source={require('../assets/card-images/2D.png')}
+  />,
+  '2H': <Image
+    style={styles.cardStyle}
+    source={require('../assets/card-images/2H.png')}
+  />,
+  '2S': <Image
+    style={styles.cardStyle}
+    source={require('../assets/card-images/2S.png')}
+  />,
+  '3C': <Image
+    style={styles.cardStyle}
+    source={require('../assets/card-images/3C.png')}
+  />,
+  '3D': <Image
+    style={styles.cardStyle}
+    source={require('../assets/card-images/3D.png')}
+  />,
+  '3H': <Image
+    style={styles.cardStyle}
+    source={require('../assets/card-images/3H.png')}
+  />,
+  '3S': <Image
+    style={styles.cardStyle}
+    source={require('../assets/card-images/3S.png')}
+  />,
+  '4C': <Image
+    style={styles.cardStyle}
+    source={require('../assets/card-images/4C.png')}
+  />,
+  '4D': <Image
+    style={styles.cardStyle}
+    source={require('../assets/card-images/4D.png')}
+  />,
+  '4H': <Image
+    style={styles.cardStyle}
+    source={require('../assets/card-images/4H.png')}
+  />,
+  '4S': <Image
+    style={styles.cardStyle}
+    source={require('../assets/card-images/4S.png')}
+  />,
+  '5C': <Image
+    style={styles.cardStyle}
+    source={require('../assets/card-images/5C.png')}
+  />,
+  '5D': <Image
+    style={styles.cardStyle}
+    source={require('../assets/card-images/5D.png')}
+  />,
+  '5H': <Image
+    style={styles.cardStyle}
+    source={require('../assets/card-images/5H.png')}
+  />,
+  '5S': <Image
+    style={styles.cardStyle}
+    source={require('../assets/card-images/5S.png')}
+  />,
+  '6C': <Image
+    style={styles.cardStyle}
+    source={require('../assets/card-images/6C.png')}
+  />,
+  '6D': <Image
+    style={styles.cardStyle}
+    source={require('../assets/card-images/6D.png')}
+  />,
+  '6H': <Image
+    style={styles.cardStyle}
+    source={require('../assets/card-images/6H.png')}
+  />,
+  '6S': <Image
+    style={styles.cardStyle}
+    source={require('../assets/card-images/6S.png')}
+  />,
+  '7C': <Image
+    style={styles.cardStyle}
+    source={require('../assets/card-images/7C.png')}
+  />,
+  '7D': <Image
+    style={styles.cardStyle}
+    source={require('../assets/card-images/7D.png')}
+  />,
+  '7H': <Image
+    style={styles.cardStyle}
+    source={require('../assets/card-images/7H.png')}
+  />,
+  '7S': <Image
+    style={styles.cardStyle}
+    source={require('../assets/card-images/7S.png')}
+  />,
+  '8C': <Image
+    style={styles.cardStyle}
+    source={require('../assets/card-images/8C.png')}
+  />,
+  '8D': <Image
+    style={styles.cardStyle}
+    source={require('../assets/card-images/8D.png')}
+  />,
+  '8H': <Image
+    style={styles.cardStyle}
+    source={require('../assets/card-images/8H.png')}
+  />,
+  '8S': <Image
+    style={styles.cardStyle}
+    source={require('../assets/card-images/8S.png')}
+  />,
+  '9C': <Image
+    style={styles.cardStyle}
+    source={require('../assets/card-images/9C.png')}
+  />,
+  '9D': <Image
+    style={styles.cardStyle}
+    source={require('../assets/card-images/9D.png')}
+  />,
+  '9H': <Image
+    style={styles.cardStyle}
+    source={require('../assets/card-images/9H.png')}
+  />,
+  '9S': <Image
+    style={styles.cardStyle}
+    source={require('../assets/card-images/9S.png')}
+  />,
+  '10C': <Image
+    style={styles.cardStyle}
+    source={require('../assets/card-images/10C.png')}
+  />,
+  '10D': <Image
+    style={styles.cardStyle}
+    source={require('../assets/card-images/10D.png')}
+  />,
+  '10H': <Image
+    style={styles.cardStyle}
+    source={require('../assets/card-images/10H.png')}
+  />,
+  '10S': <Image
+    style={styles.cardStyle}
+    source={require('../assets/card-images/10S.png')}
+  />,
+  'JC': <Image
+    style={styles.cardStyle}
+    source={require('../assets/card-images/JC.png')}
+  />,
+  'JD': <Image
+    style={styles.cardStyle}
+    source={require('../assets/card-images/JD.png')}
+  />,
+  'JH': <Image
+    style={styles.cardStyle}
+    source={require('../assets/card-images/JH.png')}
+  />,
+  'JS': <Image
+    style={styles.cardStyle}
+    source={require('../assets/card-images/JS.png')}
+  />,
+  'QC': <Image
+    style={styles.cardStyle}
+    source={require('../assets/card-images/QC.png')}
+  />,
+  'QD': <Image
+    style={styles.cardStyle}
+    source={require('../assets/card-images/QD.png')}
+  />,
+  'QH': <Image
+    style={styles.cardStyle}
+    source={require('../assets/card-images/QH.png')}
+  />,
+  'QS': <Image
+    style={styles.cardStyle}
+    source={require('../assets/card-images/QS.png')}
+  />,
+  'KC': <Image
+    style={styles.cardStyle}
+    source={require('../assets/card-images/KC.png')}
+  />,
+  'KD': <Image
+    style={styles.cardStyle}
+    source={require('../assets/card-images/KD.png')}
+  />,
+  'KH': <Image
+    style={styles.cardStyle}
+    source={require('../assets/card-images/KH.png')}
+  />,
+  'KS': <Image
+    style={styles.cardStyle}
+    source={require('../assets/card-images/KS.png')}
+  />,
   'AC': <Image
     style={styles.cardStyle}
     source={require('../assets/card-images/AC.png')}
@@ -42,10 +236,7 @@ const cardImages = {
 }
 
 const Card: React.FC<Props> = ({ rank, suit }) => {
-
-
-
-  const rankSuit: 'AC' | 'AD' | 'AH' | 'AS' = `${rank}${suit}`
+  const rankSuit: rankSuitType = `${rank}${suit}`
   return (
     <View style={styles.container}>
       {cardImages[rankSuit]}
