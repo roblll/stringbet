ba034b22dc7add2d93b56ae08a51e46af1564176
rob <lenon.rob@gmail.com>
2021-07-25 15:04:47 -0500

    Add ScrollPicker.tsx

---
+++ components/ScrollPicker.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const ScrollPicker = () => {
+  return (
+    <View>
+      <Text>ScrollPicker</Text>
+    </View>
+  )
+}
+
+export default ScrollPicker;
