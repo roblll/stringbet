d80e41ac34b1db271cf9366a294201d2496b0cba
rob <lenon.rob@gmail.com>
2021-07-28 17:48:20 -0500

    Update ScrollPicker.tsx to use Picker

--- components/ScrollPicker.tsx
+++ components/ScrollPicker.tsx
@@ -1,5 +1,10 @@
 import React from 'react';
-import { View, Text, ScrollView } from 'react-native';
+import { View, StyleSheet, Dimensions } from 'react-native';
+
+import { Picker } from '@davidgovea/react-native-wheel-datepicker';
+
+const width = Dimensions.get('window').width;
+const height = width;
 
 const data: number[] = []
 for (let i = 1; i <= 169; i++) {
@@ -8,15 +13,30 @@ for (let i = 1; i <= 169; i++) {
 
 const ScrollPicker = () => {
   return (
-    <View>
-      <ScrollView
-        bounces={false}
-        showsVerticalScrollIndicator={false}
-      >
-        {data.map(info => <Text key={info}>{info}</Text>)}
-      </ScrollView>
+    <View style={styles.container}>
+      <Picker
+        style={styles.picker}
+        selectedValue={1}
+        pickerData={data}
+        onValueChange={value => { console.log(value); }}
+      />
     </View>
   )
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    width: width,
+    height: height,
+  },
+  picker: {
+    width: '100%',
+    height: '100%',
+    backgroundColor: '#23A484',
+  }
+});
 
 export default ScrollPicker;
