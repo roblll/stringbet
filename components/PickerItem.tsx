6bb32a09dceecdc579e0af697dcaab8c88249000
rob <lenon.rob@gmail.com>
2021-07-30 14:42:11 -0500

    Update PickerItem.tsx to add style prop and pass itemHeight in ScrollPicker

--- components/PickerItem.tsx
+++ components/PickerItem.tsx
@@ -3,11 +3,14 @@ import { View, Text } from 'react-native';
 
 export type Props = {
   label: string;
+  style: {
+    height: number
+  };
 }
 
-const PickerItem: React.FC<Props> = ({ label }) => {
+const PickerItem: React.FC<Props> = ({ label, style }) => {
   return (
-    <View style={{}}>
+    <View style={{ height: style.height}}>
       <Text>
         {label}
       </Text>
--- components/ScrollPicker.tsx
+++ components/ScrollPicker.tsx
@@ -32,7 +32,16 @@ const ScrollPicker: React.FC<Props> = ({
         showsHorizontalScrollIndicator={false}
         snapToInterval={itemHeight}
       >
-        <PickerItem label='1' />
+        <PickerItem label='1' style={{ height: itemHeight}} />
+        <PickerItem label='2' style={{ height: itemHeight}} />
+        <PickerItem label='3' style={{ height: itemHeight}} />
+        <PickerItem label='4' style={{ height: itemHeight}} />
+        <PickerItem label='5' style={{ height: itemHeight}} />
+        <PickerItem label='6' style={{ height: itemHeight}} />
+        <PickerItem label='7' style={{ height: itemHeight}} />
+        <PickerItem label='8' style={{ height: itemHeight}} />
+        <PickerItem label='9' style={{ height: itemHeight}} />
+        <PickerItem label='10' style={{ height: itemHeight}} />
       </ScrollView>
     </View>
   )
