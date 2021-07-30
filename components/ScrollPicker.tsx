f5a32a2ca17cd12bdfbe98928dece69e42710b84
rob <lenon.rob@gmail.com>
2021-07-30 13:44:02 -0500

    Update ScrollPicker.tsx to calculate itemHeight

--- components/ScrollPicker.tsx
+++ components/ScrollPicker.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View, StyleSheet, Dimensions, Text } from 'react-native';
+import { View, StyleSheet, Dimensions, Text, Platform } from 'react-native';
 
 const width = Dimensions.get('window').width;
 const height = width;
@@ -10,11 +10,21 @@ for (let i = 1; i <= 169; i++) {
 }
 
 export type Props = {
-  height: number,
-  width: number,
+  height?: number,
+  width?: number,
+  transparentItemRows?: number
 }
 
-const ScrollPicker: React.FC<Props> = ({ height = 300, width = 300 }) => {
+const ScrollPicker: React.FC<Props> = ({ 
+  height = 300, 
+  width = 300, 
+  transparentItemRows = 3 
+}) => {
+  let itemHeight = height / (transparentItemRows * 2 + 1);
+  if (Platform.OS === 'ios') {
+    itemHeight = Math.ceil(itemHeight);
+  }
+
   return (
     <View style={styles.container}>
       <Text>ScrollPicker</Text>
