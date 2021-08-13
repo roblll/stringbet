2fddff5ed95df6fcd4e7cbaaef58c24fd9705b7e
rob <lenon.rob@gmail.com>
2021-08-13 13:51:39 -0500

    Update ChipButton.tsx to add image and style

--- components/ChipButton.tsx
+++ components/ChipButton.tsx
@@ -1,12 +1,26 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Image, StyleSheet, Dimensions } from 'react-native';
+
+const width = Dimensions.get('window').width * .25;
 
 const ChipButton = () => {
   return (
     <View>
-      <Text>Button</Text>
+      <Image style={styles.chipImage} source={require('../assets/chip.png')} />
     </View>
   )
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  chipImage: {
+    width: width,
+    height: width,
+  },
+})
 
 export default ChipButton;
