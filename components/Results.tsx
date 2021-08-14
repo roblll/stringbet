132aec2453d5f49b31f889dfb564ed4d44bcaa09
rob <lenon.rob@gmail.com>
2021-08-14 14:09:43 -0500

    Update Results.tsx to add background and update content style

--- components/Results.tsx
+++ components/Results.tsx
@@ -2,7 +2,8 @@ import React from 'react';
 import { View, Text, StyleSheet, Dimensions } from 'react-native';
 import { MaterialIcons } from '@expo/vector-icons'; 
 
-const size = Dimensions.get('window').width / 2;
+const width = Dimensions.get('window').width;
+const height = Dimensions.get('window').height;
 
 type Props = {
   result: boolean;
@@ -17,6 +18,7 @@ const Result: React.FC<Props> = ({ result, hand, rank, percentage }) => {
                <MaterialIcons name="close" size={24} color="#FC6E51" />
   return (
     <View style={styles.container}>
+      <View style={styles.bg}></View>
       <View style={styles.content}>
         {icon}
         <Text style={styles.text}>
@@ -35,17 +37,26 @@ const styles = StyleSheet.create({
     alignItems: 'center',
   },
   content: {
-    height: size,
-    width: size,
+    position: 'absolute',
+    height: width / 4,
+    width: width / 2,
     backgroundColor: '#fff',
     borderColor: '#0d7854',
     borderWidth: 1,
+    borderRadius: width / 55,
     justifyContent: 'center',
     alignItems: 'center'
   },
   text: {
     margin: 10,
     textAlign: 'center',
+  },
+  bg: {
+    flex: 1,
+    backgroundColor: '#000',
+    opacity: .25,
+    height: height,
+    width: width,
   }
 })
 
