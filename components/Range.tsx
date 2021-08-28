8dc70b471a8b070a478d5f04de09317cdd70e16e
rob <lenon.rob@gmail.com>
2021-08-28 18:44:44 -0500

    Add Range.tsx

---
+++ components/Range.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const Range = () => {
+  return (
+    <View>
+      <Text>Range</Text>
+    </View>
+  )
+}
+
+export default Range;
