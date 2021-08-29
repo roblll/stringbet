a4d55ed384aa225139dd2fb05d311e4cea357146
rob <lenon.rob@gmail.com>
2021-08-28 21:07:01 -0500

    Update Range.tsx to add style

--- components/Range.tsx
+++ components/Range.tsx
@@ -1,6 +1,11 @@
 import React from 'react';
-import { View, Text, Button } from 'react-native';
+import { View, Text, Button, Dimensions, StyleSheet } from 'react-native';
 import ScrollPicker, { dataType } from '../components/ScrollPicker';
+
+const width = Dimensions.get('window').width;
+const height = Dimensions.get('window').height;
+const contentWidth = width * .55;
+const contentHeight = contentWidth * 691 / 1056;
 
 type Props = {
   hide: () => void;
@@ -18,13 +23,62 @@ for (let i = 1; i <= 169; i++) {
 
 const Range: React.FC<Props> = ({ hide }) => {
   return (
-    <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
-      <Text>Range</Text>
-      <ScrollPicker data={minData} setPick={() => {}} />
-      <ScrollPicker data={maxData} setPick={() => {}} />
-      <Button title='X' onPress={hide} />
+    <View style={styles.container}>
+      <View style={styles.bg} />
+      <View style={styles.content}>
+        <Text>Range</Text>
+        <View style={{flex: 1, flexDirection: 'row'}}>
+          <ScrollPicker 
+            data={minData} 
+            setPick={() => {}} 
+            transparentItemRows={1}
+            height={100}
+            width={100}
+          />
+          <ScrollPicker 
+            data={maxData} 
+            setPick={() => {}} 
+            transparentItemRows={1}
+            height={100}
+            width={100}
+          />
+        </View>
+        <Button title='X' onPress={hide} />
+      </View>
     </View>
   )
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  content: {
+    position: 'absolute',
+    height: contentHeight,
+    width: contentWidth,
+    backgroundColor: '#fff',
+    borderColor: '#0d7854',
+    borderWidth: 1,
+    borderRadius: width / 50,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  text: {
+    margin: 10,
+    textAlign: 'center',
+    fontSize: contentHeight / 9,
+    fontWeight: '400',
+  },
+  bg: {
+    flex: 1,
+    backgroundColor: '#000',
+    opacity: .25,
+    height: height,
+    width: width,
+  }
+})
 
 export default Range;
