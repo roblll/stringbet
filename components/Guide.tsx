c6cfc980522295773e150a1f57737db451927c49
rob <lenon.rob@gmail.com>
2021-09-01 13:43:22 -0500

    Update Guide.tsx to use card font for title

--- components/Guide.tsx
+++ components/Guide.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { View, Text, StyleSheet, Dimensions, ScrollView, TouchableOpacity } from 'react-native';
+import { useFonts } from 'expo-font';
 
 import MenuButton from './MenuButton';
 
@@ -15,22 +16,30 @@ type Props = {
 }
 
 const Guide: React.FC<Props> = ({ hide }) => {
-  return (
-    <View style={styles.container}>
-      <View style={styles.bg}></View>
-      <View style={styles.content}>
-        <Text style={styles.title}>Guide</Text>
-        <ScrollView showsVerticalScrollIndicator={false}>
-          {rankings.map((item, index) => 
-            <Text style={styles.listItem} key={index}>{`${item} is ${index + 1} (${getHandPercentage(index + 1)}%) `}</Text>
-          )}
-        </ScrollView>
-        <View style={styles.close}>
-          <MenuButton title="EXIT" onPress={hide} />
+  let [fontsLoaded] = useFonts({
+    'CardC': require('../assets/fonts/CARDC___.otf'),
+  });
+
+  if (!fontsLoaded) {
+    return <View></View>
+  } else {
+    return (
+      <View style={styles.container}>
+        <View style={styles.bg}></View>
+        <View style={styles.content}>
+          <Text style={styles.title}>Guide</Text>
+          <ScrollView showsVerticalScrollIndicator={false}>
+            {rankings.map((item, index) => 
+              <Text style={styles.listItem} key={index}>{`${item} is ${index + 1} (${getHandPercentage(index + 1)}%) `}</Text>
+            )}
+          </ScrollView>
+          <View style={styles.close}>
+            <MenuButton title="EXIT" onPress={hide} />
+          </View>
         </View>
       </View>
-    </View>
-    )
+      )
+  }
 }
 
 const styles = StyleSheet.create({
@@ -59,6 +68,8 @@ const styles = StyleSheet.create({
     textAlign: 'center',
     fontSize: 18,
     margin: 10,
+    fontFamily: 'CardC',
+    marginBottom: 15,
   },
   listItem: {
     textAlign: 'center',
