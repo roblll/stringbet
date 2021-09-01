2eb75943bdb645b78c3443222725d987be3aa894
rob <lenon.rob@gmail.com>
2021-09-01 14:04:23 -0500

    Update PickerItem.tsx to use card font

--- components/PickerItem.tsx
+++ components/PickerItem.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { View, Text } from 'react-native';
+import { useFonts } from 'expo-font';
 
 export type Props = {
   key: number;
@@ -10,22 +11,31 @@ export type Props = {
 }
 
 const PickerItem: React.FC<Props> = ({ label, style }) => {
-  return (
-    <View style={{ 
-      height: style.height, 
-      alignItems: 'center', 
-      justifyContent: 'center',
-    }}>
-      <Text
-        style={{
-          color: '#093b2c',
-          fontWeight: '500',
-        }}
-      >
-        {label}
-      </Text>
-    </View>
-  );
+  let [fontsLoaded] = useFonts({
+    'CardC': require('../assets/fonts/CARDC___.otf'),
+  });
+
+  if (!fontsLoaded) {
+    return <View></View>
+  } else {
+    return (
+      <View style={{ 
+        height: style.height, 
+        alignItems: 'center', 
+        justifyContent: 'center',
+      }}>
+        <Text
+          style={{
+            fontFamily: 'CardC',
+            color: '#093b2c',
+            fontWeight: '500',
+          }}
+        >
+          {label}
+        </Text>
+      </View>
+    );
+  }
 }
 
 export default PickerItem;
