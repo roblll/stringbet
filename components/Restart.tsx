d7fa4e690455f61020565af22d80eb9da93d63e1
rob <lenon.rob@gmail.com>
2021-11-05 17:16:24 -0500

    Update Restart.tsx to add style

--- components/Restart.tsx
+++ components/Restart.tsx
@@ -1,12 +1,50 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, StyleSheet, Dimensions } from 'react-native';
+
+const width = Dimensions.get('window').width;
+const height = Dimensions.get('window').height;
+const contentWidth = width * .55;
+const contentHeight = contentWidth * 1056 / 691;
 
 const Restart = () => {
   return (
-    <View>
-      <Text>Restart</Text>
+    <View style={styles.container}>
+      <View style={styles.bg} />
+      <View style={styles.content}>
+        <Text style={styles.title}>RESTART</Text>
+      </View>
     </View>
   )
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  content: {
+    position: 'absolute',
+    height: contentHeight,
+    width: contentWidth,
+    backgroundColor: '#fff',
+    borderColor: '#0d7854',
+    borderWidth: 1,
+    borderRadius: width / 30,
+  },
+  bg: {
+    flex: 1,
+    backgroundColor: '#000',
+    opacity: .25,
+    height: height,
+    width: width,
+  },
+  title: {
+    textAlign: 'center',
+    fontSize: 18,
+    margin: 10,
+    fontFamily: 'CardC',
+  },
+})
 
 export default Restart;
