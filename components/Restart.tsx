7fe7e8da21afa7ae99740f47028096158534c165
rob <lenon.rob@gmail.com>
2021-11-05 16:54:38 -0500

    Add /components/Restart.tsx

---
+++ components/Restart.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const Restart = () => {
+  return (
+    <View>
+      <Text>Restart</Text>
+    </View>
+  )
+}
+
+export default Restart;
